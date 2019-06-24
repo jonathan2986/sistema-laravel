ac2cc2d67f946558888fc976a67c589864e74bce
jonathan <jonathan.joel2905@gmail.com>
2019-06-24 01:28:51 -0400

    Primer commit

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Categoria;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categoria = Categoria::all();
+        return $categoria;
+    }
+
+    
+   
+    public function store(Request $request)
+    {
+        $categoria = new Categoria();
+        $categoria->nombre = $request->nombre;
+        $categoria->descripcion = $request->descripcion;
+        $categoria->condicion = '1';
+        $categoria->save();
+    }
+
+
+    public function update(Request $request)
+    {
+        $categoria = Categoria::findOrFail($request->id);
+        $categoria->nombre = $request->nombre;
+        $categoria->descripcion = $request->descripcion;
+        $categoria->condicion = '1';
+        $categoria->save();
+        
+    }
+
+      public function desactivar(Request $request)
+    {
+        $categoria = Categoria::findOrFail($request->id);
+        $categoria->condicion = '0';
+        $categoria->save();
+    }
+
+     public function activar(Request $request)
+    {
+        $categoria = Categoria::findOrFail($request->id);
+        $categoria->condicion = '1';
+        $categoria->save();
+    }
+
+
+
+}
